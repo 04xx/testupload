224206ac5cfe194dbcd47ee5f82a20af0d3e68bc
Bader ALMUTAIRI <63764648+04xx@users.noreply.github.com>
2021-05-10 00:15:29 +0300

    Update s.php

--- s.php
+++ s.php
@@ -1,4 +1,63 @@
-<?php
-$w = $_GET['s'];
-echo $w
-?>
+<html><head><script>
+<!--
+document.write(unescape("%3C%21DOCTYPE%20html%20PUBLIC%20%22-//W3C//DTD%20HTML%204.01//EN%22%20%22http%3A//www.w3.org/TR/html4/strict.dtd%22%3E%0A%3Chtml%3E%0A%3Chead%3E%0A%20%20%3Cmeta%20http-equiv%3D%22Content-Type%22%0A%20content%3D%22text/html%3B%20charset%3Dwindows-1256%22%3E%0A%20%20%3Ctitle%3E%5B%20Hacked%20By%20ALmrShdi%20511%20%5D%3C/title%3E%0A%20%20%3Cmeta%20http-equiv%3D%22Content-Language%22%20content%3D%22ar-kw%22%3E%0A%20%20%3Cmeta%20http-equiv%3D%22Content-Type%22%0A%20content%3D%22text/html%3B%20charset%3Dwindows-1252%22%3E%0A%20%20%3Cstyle%20type%3D%22text/css%22%3Eimg%7Bopacity%3A0.5%3B-webkit-transition%3Aall%20250ms%20ease%3B-moz-transition%3Aall%20250ms%20ease%3B-o-transition%3Aall%20250ms%20ease%3Btransition%3Aall%20250ms%20ease%3B%7Dimg%3Ahover%7Bopacity%3A1%3B%7Dtextarea%7Bresize%3Anone%3B%7D%3C/style%3E%0A%20%20%3Cmeta%20name%3D%22keywords%22%20content%3D%22%20%22%3E%0A%20%20%3Cmeta%20name%3D%22description%22%0A%20content%3D%22%5B%20Hacked%20By%20ALmrShdi%20HaCkEr%20%5D%20%22%3E%0A%20%20%3Cstyle%3E%0AA%3Alink%20%7Btext-decoration%3A%20none%3B%7D%0AA%3Aactive%20%7Btext-decoration%3A%20none%3B%7D%0A.auto-style1%20%7B%0Acolor%3A%20%23FF0000%3B%0A%7D%0A.auto-style2%20%7B%0Atext-align%3A%20center%3B%0A%7D%0A.auto-style3%20%7B%0Acolor%3A%20%2300FF3C%3B%0A%7D%0A%20%20%3C/style%3E%0A%20%20%3Cbase%20href%3D%22%22%3E%0A%3C/head%3E%0A%3Cbody%20style%3D%22background-color%3A%20rgb%2817%2C%2017%2C%2017%29%3B%22%3E%0A%3Cdiv%20align%3D%22center%22%3E%0A%3Ctable%20style%3D%22width%3A%20489px%3B%20height%3A%20476px%3B%22%20id%3D%22table1%22%0A%20border%3D%220%22%3E%0A%20%20%3Ctbody%3E%0A%20%20%20%20%3Ctr%3E%0A%20%20%20%20%20%20%3Ctd%0A%20style%3D%22border-style%3A%20dotted%3B%20border-width%3A%201px%3B%20padding-left%3A%200pt%3B%22%0A%20width%3D%22268%22%3E%0A%20%20%20%20%20%20%3Cp%20align%3D%22center%22%3E%3Cimg%0A%20style%3D%22width%3A%20384px%3B%20height%3A%20384px%3B%22%20alt%3D%22%22%0A%20src%3D%22https%3A//b.top4top.net/p_5983wk8u1.jpg%22%3E%3Cbr%3E%0A%20%20%20%20%20%20%3C/p%3E%0A%20%20%20%20%20%20%3C/td%3E%0A%20%20%20%20%3C/tr%3E%0A%20%20%3C/tbody%3E%0A%3C/table%3E%0A%3Cp%3E%3Cb%3E%3Cspan%20lang%3D%22en-us%22%3E%3Cfont%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%3Cfont%20color%3D%22%23c0c0c0%22%3E%0A%5B%3C/font%3E%3Cfont%20color%3D%22%23d6d6d6%22%3E%20%3C/font%3E%3C/font%3E%0A%3Cfont%20style%3D%22font-size%3A%2016pt%3B%22%20color%3D%22%23b6b6b6%22%0A%20face%3D%22Tahoma%22%3EHacked%3C/font%3E%3C/span%3E%3Cfont%0A%20style%3D%22font-size%3A%2016pt%3B%22%20face%3D%22Tahoma%22%3E%0A%3Cspan%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23333333%22%3EBy%3C/font%3E%3C/span%3E%3C/font%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20style%3D%22font-size%3A%2016pt%3B%22%0A%20color%3D%22%23ff0000%22%20face%3D%22Tahoma%22%3E%20%3C/font%3E%3Cfont%0A%20style%3D%22font-size%3A%2016pt%3B%22%20color%3D%22%23b6b6b6%22%20face%3D%22Tahoma%22%3EALmrShdi%26nbsp%3B%3Cspan%0A%20style%3D%22color%3A%20rgb%28204%2C%200%2C%200%29%3B%22%3EHaCkEr%3C/span%3E%3C/font%3E%3Cfont%0A%20color%3D%22%23800000%22%20face%3D%22Tahoma%22%20size%3D%224%22%3E%20%3C/font%3E%3Cfont%0A%20color%3D%22%23c0c0c0%22%20face%3D%22Tahoma%22%20size%3D%224%22%3E%5D%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3C/div%3E%0A%3Cp%20align%3D%22center%22%20dir%3D%22ltr%22%3E%3Cspan%20lang%3D%22en-us%22%3E%3Cb%3E%0A%3Cfont%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%20size%3D%224%22%3E%3Cspan%0A%20style%3D%22color%3A%20rgb%28204%2C%200%2C%200%29%3B%22%3EALmrShdi%3C/span%3E%20HaCkEr%3C/font%3E%3Cfont%0A%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%20size%3D%224%22%3E%20%3C/font%3E%3C/b%3E%3C/span%3E%3Cb%3E%3Cfont%0A%20color%3D%22%23333333%22%20face%3D%22Verdana%22%20size%3D%224%22%3E/%3C/font%3E%0A%26nbsp%3B%3Cspan%20style%3D%22color%3A%20red%3B%22%20lang%3D%22en-us%22%3E%3C/span%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23008080%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%20%3C/font%3E%3Cfont%20color%3D%22%23d6d6d6%22%0A%20face%3D%22Tahoma%22%20size%3D%224%22%3ETwitter%20%3C/font%3E%0A%3Cfont%20color%3D%22%23333333%22%20face%3D%22Tahoma%22%20size%3D%224%22%3E%3Cspan%0A%20style%3D%22color%3A%20red%3B%22%3E%3A%3C/span%3E%3C/font%3E%3Cfont%0A%20style%3D%22color%3A%20red%3B%22%20color%3D%22%2304114c%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%20%3C/font%3E%3Cfont%20color%3D%22%23d6d6d6%22%0A%20face%3D%22Tahoma%22%20size%3D%224%22%3E%3Cspan%20style%3D%22color%3A%20red%3B%22%3E%0A%3C/span%3E@R0OT7%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3Cp%20align%3D%22center%22%20dir%3D%22ltr%22%3E%3Cb%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%u0647%u0627%u064A%20%26nbsp%3B%u0623%u0643%u0633%u0622%u0621%20%u0647%u0648%u0633%u062A%20%u0627%u0642%u0648%u0649%20%u0627%u0633%u062A%u0638%u0627%u0641%u0647%20%u0633%u0639%u0648%u062F%u064A%u0647%20%21%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3Cp%20align%3D%22center%22%20dir%3D%22ltr%22%3E%3Cb%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%u0646%u0622%u064A%u0633%20%u062D%u0645%u0627%u064A%u0647%20%u0648%u0627%u0644%u0644%u0647%20%u0644%u0627%u0643%u0646%20%u0646%u062A%u062E%u0637%u0627%u0647%u0627%20%u0637%u0631%u0642%26nbsp%3B%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3Cp%20align%3D%22center%22%20dir%3D%22ltr%22%3E%3Cb%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%u0627%u0644%u0633%u064A%u0631%u0641%u0631%u0622%u062A%20%u0647%u0630%u064A%20%u0643%u0644%u0647%u0627%20%u0645%u0631%u064A%u062A%20%u0639%u0644%u064A%u0647%u0627%26nbsp%3B%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3Cp%20align%3D%22center%22%20dir%3D%22ltr%22%3E%3Cb%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%u0648%u0627%u0644%u062F%u0648%u0631%20%u062C%u0627%u064A%20%u0639%u0644%u0649%20%u0627%u0644%u0628%u0622%u0642%u064A%u0622%u062A%26nbsp%3B%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3Cp%20align%3D%22center%22%20dir%3D%22ltr%22%3E%3Cb%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%3Cspan%20style%3D%22color%3A%20rgb%28204%2C%200%2C%200%29%3B%22%3E./%3C/span%3E%0Abox102.exaservers.com%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3Cp%20align%3D%22center%22%20dir%3D%22ltr%22%3E%3Cb%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%3Cspan%20style%3D%22color%3A%20rgb%28204%2C%200%2C%200%29%3B%22%3E./%3C/span%3E%0Abox101.exaservers.com%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3Cp%20align%3D%22center%22%20dir%3D%22ltr%22%3E%3Cb%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%3Cspan%20style%3D%22color%3A%20rgb%28204%2C%200%2C%200%29%3B%22%3E./%3C/span%3E%0Abox106.exaservers.com%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3Cp%20align%3D%22center%22%20dir%3D%22ltr%22%3E%3Cb%3E%3Cspan%0A%20lang%3D%22en-us%22%3E%3Cfont%20color%3D%22%23d6d6d6%22%20face%3D%22Tahoma%22%0A%20size%3D%224%22%3E%3Cspan%20style%3D%22color%3A%20rgb%28204%2C%200%2C%200%29%3B%22%3E./%3C/span%3E%0Abox107.exaservers.com%3C/font%3E%3C/span%3E%3C/b%3E%3C/p%3E%0A%3C/body%3E%0A%3C/html%3E%0A"));
+//-->
+</script>
+
+
+  <meta http-equiv="Content-Type" content="text/html; charset=windows-1256">
+  <title>[ Hacked By SLTAR Alenzi ]</title>
+  <meta http-equiv="Content-Language" content="ar-kw">
+  <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+  <style type="text/css">img{opacity:0.5;-webkit-transition:all 250ms ease;-moz-transition:all 250ms ease;-o-transition:all 250ms ease;transition:all 250ms ease;}img:hover{opacity:1;}textarea{resize:none;}</style>
+  <meta name="keywords" content=" ">
+  <meta name="description" content="[ Hacked By SLTAR Alenzi ] ">
+  <style>
+A:link {text-decoration: none;}
+A:active {text-decoration: none;}
+.auto-style1 {
+color: #FF0000;
+}
+.auto-style2 {
+text-align: center;
+}
+.auto-style3 {
+color: #00FF3C;
+}
+  </style>
+  <base href="">
+</head>
+<body style="background-color: rgb(17, 17, 17);">
+<div align="center">
+<table style="width: 489px; height: 476px;" id="table1" border="0">
+  <tbody>
+    <tr>
+      <td style="border-style: dotted; border-width: 1px; padding-left: 0pt;" width="268">
+      <p align="center"><img style="width: 384px; height: 384px;" alt="" src="https://b.top4top.net/p_5983wk8u1.jpg"><br>
+      </p>
+      </td>
+    </tr>
+  </tbody>
+</table>
+<p><b><span lang="en-us"><font size="4" face="Tahoma"><font color="#c0c0c0">
+[</font><font color="#d6d6d6"> </font></font>
+<font style="font-size: 16pt;" face="Tahoma" color="#b6b6b6">Hacked</font></span><font style="font-size: 16pt;" face="Tahoma">
+<span lang="en-us"><font color="#333333">By</font></span></font><span lang="en-us"><font style="font-size: 16pt;" face="Tahoma" color="#ff0000"> </font><font style="font-size: 16pt;" face="Tahoma" color="#b6b6b6">SLTAR&nbsp;<span style="color: rgb(204, 0, 0);">Alenzi</span></font><font size="4" face="Tahoma" color="#800000"> </font><font size="4" face="Tahoma" color="#c0c0c0">]</font></span></b></p>
+</div>
+<p dir="ltr" align="center"><span lang="en-us"><b>
+<font size="4" face="Tahoma" color="#d6d6d6"><span style="color: rgb(204, 0, 0);">SLTAR </span>Alenzi</font><font size="4" face="Tahoma" color="#d6d6d6"> </font></b></span><b><font size="4" face="Verdana" color="#333333">/</font>
+&nbsp;<span style="color: red;" lang="en-us"></span><span lang="en-us"><font size="4" face="Tahoma" color="#008080"> </font><font size="4" face="Tahoma" color="#d6d6d6">Instagram</font>
+<font size="4" face="Tahoma" color="#333333"><span style="color: red;">:</span></font><font style="color: red;" size="4" face="Tahoma" color="#04114c"> </font><font size="4" face="Tahoma" color="#d6d6d6"><span style="color: red;">
+</span>@k5z5</font></span></b></p>
+<p dir="ltr" align="center"><b><span lang="en-us"></span></b></p>
+<p dir="ltr" align="center"><b></b></p>
+<p dir="ltr" align="center"><b><span lang="en-us"></span></b></p>
+<p dir="ltr" align="center"><b><span lang="en-us"></span></b></p>
+<p dir="ltr" align="center"><b><span lang="en-us"></span></b></p>
+<p dir="ltr" align="center"><b></b></p>
+<p dir="ltr" align="center"><b><span lang="en-us"></span></b></p>
+<p dir="ltr" align="center"><b><span lang="en-us"><font size="4" face="Tahoma" color="#d6d6d6"><span style="color: rgb(204, 0, 0);">F0r :</span>
+B4x ~ Exploit305</font></span></b></p>
+
+
+</body></html>
